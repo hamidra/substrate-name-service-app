6e9fe9ae1865d5a81b717b0ba22c30d07cc5583b
hamidra <hamid.alipour@gmail.com>
2022-03-11 15:09:00 -0800

    refactor navbar button

--- src/components/ButtonNavbar.tsx
+++ src/components/ButtonNavbar.tsx
@@ -10,30 +10,32 @@ interface tab {
   title: string;
   path: string;
 }
-const NameButtonNavbar = ({ tabs }: { tabs: tab[] }) => {
+const NavTabButton = ({ tab }) => {
   let navigate = useNavigate();
 
   let switchActiveTab = (tab: tab) => {
     tab && navigate(tab.path);
   };
+
+  let match = useMatch(tab.path);
+  let isActive = !!match;
+  return (
+    <button
+      style={{ minWidth: 100 }}
+      className={`btn ${isActive ? 'btn-primary' : 'btn-outline-primary'}`}
+      onClick={() => switchActiveTab(tab)}
+    >
+      {tab.title}
+    </button>
+  );
+};
+
+const NameButtonNavbar = ({ tabs }: { tabs: tab[] }) => {
   return (
     <div className="btn-group" role="group" aria-label="Button Navigation Bar">
-      {tabs.map((tab, index) => {
-        let match = useMatch(tab.path);
-        let isActive = !!match;
-        return (
-          <button
-            style={{ minWidth: 100 }}
-            key={index}
-            className={`btn ${
-              isActive ? 'btn-primary' : 'btn-outline-primary'
-            }`}
-            onClick={() => switchActiveTab(tab)}
-          >
-            {tab.title}
-          </button>
-        );
-      })}
+      {tabs.map((tab, index) => (
+        <NavTabButton tab={tab} key={index} />
+      ))}
     </div>
   );
 };

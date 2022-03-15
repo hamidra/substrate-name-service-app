97c3aa957fcf6acd927ec61594e9d0ce63504fa6
hamidra <hamid.alipour@gmail.com>
2022-03-15 17:51:12 -0400

    add namehash and salt utilities

--- src/substrate/palletProvider.ts
+++ src/substrate/palletProvider.ts
@@ -1,6 +1,18 @@
 import { blake2AsHex } from '@polkadot/util-crypto';
+import { numberToU8a, stringToU8a } from '@polkadot/util';
 
-export const generateNameHash = (name: string, secret: Number) => {
-  const hash = blake2AsHex(name);
-  console.log(hash);
+export const getSaltU64 = () => {
+  return Math.floor(Math.random() * 2 ** 64);
+};
+
+export const generateNameHash = (name: string, secret: number) => {
+  const nameU8a: Uint8Array = stringToU8a(name);
+  const secretU8a: Uint8Array = numberToU8a(secret);
+  const saltedNameU8a: Uint8Array = new Uint8Array(
+    nameU8a.length + secretU8a.length
+  );
+  saltedNameU8a.set(nameU8a);
+  saltedNameU8a.set(secretU8a, nameU8a.length);
+  const hash = blake2AsHex(saltedNameU8a);
+  return hash;
 };

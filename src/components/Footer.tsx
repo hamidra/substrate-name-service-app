8569f033eb2ec62e32aaa60ba9936df6ad6d691b
hamidra <hamid.alipour@gmail.com>
2022-04-25 09:18:57 -0700

    add footer + cleanup unused

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+
+interface FooterProps {
+  className?: string;
+}
+
+export default function Footer({ className }: FooterProps) {
+  return (
+    <footer
+      className={`${className} footer d-flex flex-column flex-md-row px-4`}
+    >
+      <div className="d-flex flex-column flex-sm-row">
+        <div>
+          © {new Date().getFullYear()} All rights reserved.
+          <span className="d-none d-sm-inline">&nbsp;&nbsp;·&nbsp;&nbsp;</span>
+        </div>
+        <div>
+          <span>
+            <Link to="/privacy-policy">Privacy Policy</Link>
+          </span>
+        </div>
+      </div>
+      <div className="footer-grow flex-grow-1" />
+      <div>
+        <strong>Questions?</strong>&nbsp;
+        <a
+          href="https://support.polkadot.network/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Visit our support page.
+        </a>
+      </div>
+    </footer>
+  );
+}
